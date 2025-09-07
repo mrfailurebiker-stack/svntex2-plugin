903ef153506efa82c2729ed3eb743ce211f8192f
Mac <mac@NITHIN-MacBook-Air.local>
2025-09-07 18:25:18 +0530

    Add KYC testing code

---
+++ .php
@@ -0,0 +1,9 @@
+
+<?php
+// Testing code: Generate and print a random number between 1 and 100
+$randomNumber = rand(1, 100);
+echo "<h1>Testing: Random Number = $randomNumber</h1>";
+?>
+$randomNumber = rand(1, 100);
+echo $randomNumber;
+?>
