9e8e07b19c4f522ad29d8dd6f216ec040ab3f9f5
Mac <mac@NITHIN-MacBook-Air.local>
2025-09-09 05:54:18 +0530

    Feature: Add custom SVNTeX-only logout handler and update dashboard links

--- views/dashboard.php
+++ views/dashboard.php
@@ -18,7 +18,7 @@ if ( function_exists('wc_get_orders') ) {
     ]);
 }
 
-$logout_url = esc_url( wp_logout_url( home_url('/') ) . '&_wpnonce=' . wp_create_nonce('log-out') );
+$logout_url = esc_url( home_url('/wp-content/plugins/svntex2-plugin/logout-handler.php') );
 ?>
 <div class="svntex-dash-top">
     <a href="<?php echo esc_url( home_url('/') ); ?>" class="dash-brand">SVNTeX</a>
